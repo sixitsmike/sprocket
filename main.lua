85e8dd91eacf9bb2a3a6a19c240ddec7b9af10e3
regniraj <regniraj@gmail.com>
2011-04-11 07:02:12 +0545

    changed the scale to show in windows higher resolution pics are not shown in windows

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ level_1 = require('level_1')
 displayList = display.newGroup()
 
 local function onStartGame(event)
-    
+
     background:removeSelf()
     physics.start()
     level_1:start()
@@ -14,11 +14,11 @@ end
 
 local function showSplash()
 
-    background = display.newImage( "splash.jpg", 0, 0, true )
+    background = display.newImage( "splash.png", 0, 0, true )
     background.x = display.contentWidth / 2
     background.y = display.contentHeight / 2
-    background.xScale = .55
-    background.yScale = .55
+    background.xScale = .65
+    background.yScale = .65
 
     background:addEventListener("touch", onStartGame)
 
